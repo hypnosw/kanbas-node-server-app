e981591bad08fc9b63aeb28c7f5570c841b0e014
hypnosw <hypnoswu@gmail.com>
2023-10-11 02:47:55 -0700

    working on nav

--- 2023/fall/webdev/kanbas-react-web-app/src/Kanbas/index.js
+++ 2023/fall/webdev/kanbas-react-web-app/src/Kanbas/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import Nav from "../Nav";
+import KanbasNavigation from "./KanbasNavigation";
 
 function Kanbas() {
     return(
-        <div>
-            <Nav/>
-            <h1>Kanbas</h1>
+        <div className="d-flex">
+            <KanbasNavigation/>
+            <div>
+                <h1>Account</h1>
+                <h1>Dashboard</h1>
+                <h1>Courses</h1>
+            </div>
         </div>
+
     );
 }
 export default Kanbas;
---
+++ 2023/fall/webdev/kanbas-react-web-app/src/Kanbas/kanbas-style.js
@@ -0,0 +1,27 @@
+const wdKanbasRed = {
+    color:'rgb(167, 21, 35)',
+};
+
+const wdKanbasGray = {
+    color:'#c7cdd1',
+};
+
+const wdKanbasBtnColor = {
+    color:'#f5f5f5',
+};
+
+const wdHrStyle = {
+    border:'1px solid #c7cdd1',
+    'margin-bottom':'5px',
+};
+//
+// const wdNavbarColor = {
+//     color: '#B52828',
+// };
+
+const wdListNoLine = {
+    'list-style': 'none',
+};
+
+// export {wdKanbasBtnColor, wdKanbasRed, wdKanbasGray, wdHrStyle, wdListNoLine}
+export * from 'kanbas-styles';
