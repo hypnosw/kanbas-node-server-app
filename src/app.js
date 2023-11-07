6548f6b9084ed575aedde786a069c240fb97dae1
hypnosw <hypnoswu@gmail.com>
2023-11-07 14:28:47 -0800

    starting lab section

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
-const express = require('express')
-const app = express()
-app.get('/hello', (req, res) => {res.send('Hello World!')})
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./Lab5.js";
+
+const app = express();
+Lab5(app);
+Hello(app);
 app.listen(4000)
--- src/hello.js
+++ src/hello.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+const Hello = (app)=>{
+    console.log("Hello World!");
+    app.get('/hello', (req, res) => {res.send('Hello World!')})
+    app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+};
+
+export default Hello;
