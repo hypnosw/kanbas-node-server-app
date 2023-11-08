259e641783864658c93451477c8d15c6d867a4ef
hypnosw <hypnoswu@gmail.com>
2023-11-07 17:15:46 -0800

    working on 3.3.6

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
-import Hello from "./hello.js";
-import Lab5 from "./Lab5.js";
+import Hello from "./src/hello.js";
+import Lab5 from "./src/Lab5.js";
 
 const app = express();
 Lab5(app);
 Hello(app);
+
 app.listen(4000)
