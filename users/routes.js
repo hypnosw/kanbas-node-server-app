c2ee114d8a37437d1f80ada16792141eaa833ae2
hypnosw <hypnoswu@gmail.com>
2023-12-12 19:49:59 -0800

    fixed updateUser

--- users/routes.js
+++ users/routes.js
@@ -20,7 +20,7 @@ function UserRoutes(app) {
     const updateUser = async (req, res) => {
         const { userId } = req.params;
         const status = await dao.updateUser(userId, req.body);
-        currentUser = await dao.findUserById(userId);
+        // currentUser = await dao.findUserById(userId);
         res.json(status);
     };
     const signup = async (req, res) => {
